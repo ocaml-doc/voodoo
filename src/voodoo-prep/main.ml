07bd960d23860d50a8e48a3d58a7471390afb990
Jon Ludlam <jon@recoil.org>
2021-07-13 09:55:41 +0100

    Package-specific prep fixes

--- src/voodoo-prep/main.ml
+++ src/voodoo-prep/main.ml
@@ -9,21 +9,38 @@ type actions = {
 let process_package : Fpath.t -> Package.t -> Fpath.t list -> unit =
  fun root package files ->
   let dest = Package.prep_path package in
+
+  (* Some packages produce ocaml artefacts that can't be processed with the switch's
+      ocaml compiler - most notably the secondary compiler! This switch is intended to 
+      be used to ignore those files *)
+  let process_ocaml_artefacts =
+    let (_, name, _) = package in
+    let package_blacklist = ["ocaml-secondary-compiler"] in
+    not (List.mem name package_blacklist)
+  in
+
   let foldfn fpath acc =
     let is_in_doc_dir =
       match Fpath.segs fpath with "doc" :: _ -> true | _ -> false
     in
+
+    (* Menhir puts a dune build dir into docs for some reason *)
+    let in_build_dir =
+      List.exists (fun x -> x = "_build") (Fpath.segs fpath)
+    in
+
     let _, filename = Fpath.split_base fpath in
     let ext = Fpath.get_ext filename in
     let no_ext = Fpath.rem_ext filename in
-    let is_module = List.mem ext [ ".cmt"; ".cmti"; ".cmi" ] in
+    let is_module = process_ocaml_artefacts && List.mem ext [ ".cmt"; ".cmti"; ".cmi" ] in
     let do_copy =
-      is_in_doc_dir || is_module
-      || List.mem no_ext (List.map Fpath.v [ "META"; "dune-package" ])
+      (not in_build_dir) &&
+      (is_in_doc_dir || is_module
+      || List.mem no_ext (List.map Fpath.v [ "META"; "dune-package" ]))
     in
-    let is_cma = List.mem ext [ ".cma"; ".cmxa" ] in
+    let is_cma = process_ocaml_artefacts && List.mem ext [ ".cma"; ".cmxa" ] in
     let copy =
-      if do_copy then Fpath.(root // fpath, dest // fpath) :: acc.copy
+      if  do_copy then Fpath.(root // fpath, dest // fpath) :: acc.copy
       else acc.copy
     in
     let info = if is_module then fpath :: acc.info else acc.info in

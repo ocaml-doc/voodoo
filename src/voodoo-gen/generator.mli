78474313ec663ed87c02bb7e101b3ebaf8424fd1
sabine <sabine@users.noreply.github.com>
2022-12-09 19:56:00 +0100

    Use odoc html --as-json to bring back the missing preambles (#39)
    Co-authored-by: Sabine Schmaltz <sabine@tarides.com>

--- src/voodoo-gen/generator.mli
+++
@@ -1,9 +0,0 @@
-open Odoc_document
-
-val render_content : indent:bool -> Types.Page.t -> Renderer.page
-val render_toc : indent:bool -> Types.Page.t -> Renderer.page
-
-val doc :
-  xref_base_uri:string ->
-  Types.Block.t ->
-  Html_types.flow5_without_sectioning_heading_header_footer Tyxml.Html.elt list

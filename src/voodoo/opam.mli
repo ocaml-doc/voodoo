84123a3071155d0d7abc6b609bff8a5269b11440
Guillaume Petiot <guillaume@tarides.com>
2023-07-21 16:24:45 +0100

    Simplify voodoo-do cli (#107)

--- src/voodoo/opam.mli
+++ src/voodoo/opam.mli
@@ -1,6 +1,2 @@
-type package = { name : string; version : string }
-
 val switch : string option ref
-val dependencies : package -> package list
-val all_opam_packages : unit -> package list
 val find : Package.t -> (Fpath.t, [> Bos_setup.R.msg ]) Bos_setup.result
